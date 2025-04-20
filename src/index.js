92e242bbc96b76020387e98a0ad5200bedc01ad0
md-shakib-khan <mdshakibkhan.dev@gmail.com>
2025-04-20 21:20:55 +0600

    new version 1.0.5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Button from "./Components/Buttons/Button";
 import Card from "./Components/Cards/Card";
 import Drawer from "./Components/Drawer/Drawer";
+import Modal from "./Components/Modal/Modal";
+import RippleEffect from "./Effects/Ripple/RippleEffect";
 
-export { Button, Card, Drawer };
+export { Button, Card, Drawer, Modal, RippleEffect };
